ff00094a307db48d2c65b99647f41e78c5d813f5
Masterkillerr <alvaropro.0605@gmail.com>
2026-09-23 20:55:17 -0500

    Sync desde monorepo (2026-09-24 01:55)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker para FabLab Inventario PWA
 // Cachea la app shell y permite uso offline básico.
 
-const CACHE_NAME = 'fablab-v6';
+const CACHE_NAME = 'fablab-v7';
 const SHELL_ASSETS = [
   '/',
   '/index.html',
